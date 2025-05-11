a35347642435f95fb2a1e6af5cc40c7224895e24
ViratGarg2 <virat.garg@students.iiit.ac.in>
2025-05-11 18:22:19 +0530

    deploy_fixes2

--- app.py
+++ app.py
@@ -120,5 +120,5 @@ def extract():
 
 if __name__ == '__main__':
     # Get port from environment variable for Render
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
